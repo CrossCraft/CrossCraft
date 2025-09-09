eeb7e8d10925df98c197ff10dfdc507dba9c027e
Nathan Bourgeois <iridescentrosesfall@gmail.com>
2025-09-08 21:50:05 -0400

    Base server tick

--- src/server/main.zig
+++ src/server/main.zig
@@ -6,7 +6,28 @@ pub fn main() !void {
     var server = try Server.init(std.heap.page_allocator);
     defer server.deinit(std.heap.page_allocator);
 
+    const ticks_per_second = 20;
+    const tick_duration = std.time.us_per_s / ticks_per_second;
+
+    var next_tick_time = std.time.microTimestamp() + tick_duration;
+    var tps: usize = 0;
+    var last_report_time = std.time.microTimestamp() + std.time.us_per_s;
     while (true) {
+        const now = std.time.microTimestamp();
+
+        if (now > last_report_time) {
+            std.debug.print("TPS: {d}\n", .{tps});
+            tps = 0;
+            last_report_time = now + std.time.us_per_s;
+        }
+        tps += 1;
+
+        if (now < next_tick_time) {
+            std.Thread.sleep(std.time.ns_per_us * @as(u64, @bitCast(next_tick_time - now)));
+        }
+
         server.tick();
+
+        next_tick_time = std.time.microTimestamp() + tick_duration;
     }
 }
